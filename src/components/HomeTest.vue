3abf6bf251c62af41c91ceee99a1292d25c8504a
syougo <supernaturalsyougo@gmail.com>
2023-02-09 00:14:50 +0900

    add mapGetters and mapMutations

--- src/components/HomeTest.vue
+++ src/components/HomeTest.vue
@@ -4,32 +4,51 @@
     <p>{{ storeCount }}</p>
     <p>ダブルカウント: {{ doubleCount }}</p>
     <p>トリプルカウント: {{ tripleCount }}</p>
-    <button @click="increment">プラス</button>
-    <button @click="decrement">マイナス</button>
+    <!-- <button @click="increment(2)">プラス</button>
+    <button @click="decrement(2)">マイナス</button> -->
+    <button @click="increment(2)">プラス</button>
+    <button @click="decrement(2)">マイナス</button>
 
   </div>
 </template>
 
 <script>
+  import { mapGetters } from 'vuex';
+  // import { mapMutations } from 'vuex';
+
   export default {
     computed: {
       storeCount(){
         return this.$store.state.count;
       },
-      doubleCount(){
-        return this.$store.getters.doubleCount;
-      },
-      tripleCount(){
-        return this.$store.getters.tripleCount;
-      }
+      ...mapGetters(['doubleCount', 'tripleCount'])
+      // doubleCount(){
+      //   return this.$store.getters.doubleCount;
+      // },
+      // tripleCount(){
+      //   return this.$store.getters.tripleCount;
+      // },
     },
     methods: {
       increment(){
-        this.$store.state.count ++
+        this.$store.dispatch('increment',2);
       },
       decrement(){
-        this.$store.state.count --
+        this.$store.dispatch('decrement',2);
       }
+      // increment(){
+      //   this.$store.commit('increment', 2);
+      // },
+      // decrement(){
+      //   this.$store.commit('decrement', 2);
+      // },
+      // ...mapMutations(['increment', 'decrement'])
+      // increment(){
+      //   this.$store.state.count ++
+      // },
+      // decrement(){
+      //   this.$store.state.count --
+      // }
     }
   }
 </script>
--- src/store.js
+++ src/store.js
@@ -9,6 +9,22 @@ export default new Vuex.Store({
   },
   getters: {
     doubleCount: state => state.count * 2,
-    tripleCount: state => state.count * 3,
+    tripleCount: state =>  state.count * 3,
+  },
+  mutations: {
+    increment(state, number){
+      state.count += number;
+    },
+    decrement(state, number){
+      state.count -= number;
+    }
+  },
+  actions: {
+    increment({commit}, number){
+      commit('increment', number);
+    },
+    decrement({commit}, number){
+      commit('decrement', number);
+    }
   }
 })
